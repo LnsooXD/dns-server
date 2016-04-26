e880ce0f9072f43c139fc47e60a41992978735aa
LnsooXD <LnsooXD@gmail.com>
2016-04-26 18:29:31 +0800

    change: add simple cache support

--- test/dns-transmit-service.js
+++ test/dns-transmit-service.js
@@ -13,17 +13,31 @@ const dnsAddrs = [
 ];
 
 const mod = require('../');
+const dnsCache = require('native-dns-cache');
+
 const DnsTransmitService = mod.DnsTransmitService;
 const queryDirectMiddleware = mod.getInternalMiddleware('query-from-dns');
 const queryFromTransmit = mod.getInternalMiddleware('query-from-transmit-service');
 const jsonResultPostware = mod.getInternalPostware('json-result');
-const transformResultFoDns = mod.getInternalMiddleware('adapt-result-for-dnsd');
+const transformResultFoDns = mod.getInternalPostware('adapt-result-for-dnsd');
+const queryFromCache = mod.getInternalMiddleware('query-from-dns-cache');
+const saveToCache = mod.getInternalMiddleware('save-to-dns-cache');
 
 function localTest() {
   const service = new DnsTransmitService({
     port: 3053,
     host: '127.0.0.1',
-    method: 'GET'
+    method: 'GET',
+    zone: {
+      zone: 'lnsoo.com',
+      server: 'ns.lnsoo.con',
+      admin: 'LnsooXD@gmail.com',
+      serial: 'now',
+      refresh: '2h',
+      retry: '30m',
+      expire: '2w',
+      ttl: '10m'
+    }
   });
 
   service.use(queryDirectMiddleware(dnsAddrs));
@@ -31,7 +45,7 @@ function localTest() {
   service.start();
 
   const service1 = new DnsTransmitService({
-    port: 53,
+    port: 5354,
     host: '127.0.0.1',
     method: 'DNS',
     zone: {
@@ -46,9 +60,17 @@ function localTest() {
     }
   });
 
+  let cache = new dnsCache({
+    zone: 'lnsoo.com'
+  });
+
+  service1.use(queryFromCache(cache));
   service1.use(queryFromTransmit(service));
-  service1.use(transformResultFoDns);
+  service1.use(saveToCache(cache));
+
+  service1.post(transformResultFoDns);
+
   service1.start();
 }
 
-//localTest();
+localTest();
